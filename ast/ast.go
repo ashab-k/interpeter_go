6e0444540ab73190fcdd1cc64129fe50ba1dbd20
ashab-k <ashabkhan2000@gmail.com>
2025-01-14 00:48:00 +0530

    feat:updates ast to support infix expressions

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,13 @@
 package ast
 
 import (
+	"bytes"
 	"interpreter/token"
 )
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -17,6 +19,7 @@ type Expression interface {
 	Node 
 	ExpressionNode()
 }
+
 
 type Program struct {
 	Statements []Statement
@@ -37,9 +40,53 @@ type LetStatement struct {
 	Value Expression
 }
 
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+type ReturnStatement struct {
+	Token token.Token
+	ReturnValue Expression
+}
+
+type ExpressionStatement struct {
+	Token token.Token
+	Expression Expression
+}
+
+type PrefixExpression struct {
+	Token token.Token
+	Operator string
+	Right Expression
+}
+
+func (pe *PrefixExpression) ExpressionNode() {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+func (il *IntegerLiteral) ExpressionNode() {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string { return il.Token.Literal }
+
 func (ls *LetStatement) StatementNode() {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+func (rs *ReturnStatement) StatementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+func (es *ExpressionStatement) StatementNode() {}
+
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
 type Identifier struct {
 	Token token.Token
 	Value string
@@ -47,3 +94,52 @@ type Identifier struct {
 
 func (i *Identifier) ExpressionNode() {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string { return i.Value }
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
