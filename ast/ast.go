aa231fa7541de8c1f2ba9c31142e95ab26924008
ashab-k <ashabkhan2000@gmail.com>
2025-01-14 16:39:04 +0530

    feat:updates ast to support infix expressions

--- ast/ast.go
+++ ast/ast.go
@@ -61,6 +61,27 @@ type PrefixExpression struct {
 	Right Expression
 }
 
+type InfixExpression struct {
+	Token token.Token
+	Left Expression
+	Operator string
+	Right Expression
+}
+
+func (ie *InfixExpression) ExpressionNode() {}
+func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(" " + ie.Operator + " ")
+	out.WriteString(ie.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
 func (pe *PrefixExpression) ExpressionNode() {}
 func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
 func (pe *PrefixExpression) String() string {
